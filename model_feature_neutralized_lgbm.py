1fa398a35d15c8cc99ab130c862bce81c4f1ce24
a663E-36z1120 <andrewhz.zhang@mail.utoronto.ca>
2022-01-01 17:01:40 -0500

    updated utils and added feature neutralization to model

---
+++ model_feature_neutralized_lgbm.py
@@ -0,0 +1,81 @@
+import time
+from lightgbm import LGBMRegressor
+import gc
+
+from numerapi import NumerAPI
+from halo import Halo
+from utils import *
+
+start = time.time()
+napi = NumerAPI()
+spinner = Halo(text='', spinner='dots')
+current_round = napi.get_current_round(tournament=8)  # tournament 8 is the primary Numerai Tournament
+
+download_data()
+
+# read the feature metadata and get the "small" feature set (We will need a data center grade server if we want to
+# process all the Numerai features!)
+with open("features.json", "r") as f:
+    feature_metadata = json.load(f)
+features = feature_metadata["feature_sets"]["small"]
+
+# read the training and validation data given the predefined features stored in parquets as pandas DataFrames
+training_data, validation_data = read_learning_data(features)
+# extract feature matrix and target vector used for training
+X_train = training_data.filter(like='feature_', axis='columns')
+y_train = training_data[TARGET_COL]
+# extract feature matrix and target vector used for validation
+X_val = validation_data.filter(like='feature_', axis='columns')
+y_val = validation_data[TARGET_COL]
+# "garbage collection" (gc) gets rid of unused data and frees up memory
+gc.collect()
+
+########################################################################################################################
+# define and train your model here using the loaded data sets!
+
+num_feature_neutralization = 50  # parameter for feature neutralization used after we make our predictions
+params = {"n_estimators": 50000,
+          "learning_rate": 0.00075,
+          "max_depth": 6,
+          "num_leaves": 2 ** 6,
+          "colsample_bytree": 0.5,
+          "subsample": 0.5,
+          "max_bins": 255,
+          "device": "gpu"}
+
+model_name = ("lgbm" + str(params["n_estimators"]) +
+              "-" + str(params["learning_rate"])
+              + "-" + str(params["colsample_bytree"])
+              + "-" + str(params["subsample"])
+              + "-" + f"neutral{num_feature_neutralization}").replace(".", "dot")
+model = LGBMRegressor(**params)
+
+
+spinner.start('Training model')
+model.fit(X_train, y_train)
+spinner.succeed()
+gc.collect()
+
+########################################################################################################################
+
+
+spinner.start('Predicting on validation data')
+# here we insert the predictions back into the validation data set, as we need to use the validation data set to
+# perform feature neutralization later
+validation_data.loc[:, f"preds_{model_name}"] = model.predict(X_val)
+spinner.succeed()
+gc.collect()
+
+spinner.start('Neutralizing to risky features')
+# neutralize our predictions to the k riskiest features in the training set
+neutralize_riskiest_features(training_data, validation_data, features, model_name, k=num_feature_neutralization)
+spinner.succeed()
+gc.collect()
+
+print('Exporting Predictions to csv...')
+validation_data["prediction"] = validation_data[f"preds_{model_name}_neutral_riskiest_{num_feature_neutralization}"]\
+    .rank(pct=True)
+validation_data["prediction"].to_csv(f"validation_predictions_{model_name}.csv")
+print('Done!')
+
+print(f'Time elapsed: {(time.time() - start) / 60} mins')
--- utils.py
+++ utils.py
@@ -1,20 +1,285 @@
-from pathlib import Path
-import dask.dataframe as dd
-import numerapi
+import os
+import requests
+import numpy as np
 import pandas as pd
-from dask.dataframe import DataFrame
+import scipy
+
+from pathlib import Path
 import json
-from halo import Halo
+
+import numerapi
+from scipy.stats import skew, kurtosis
+
+napi = numerapi.NumerAPI()
+
+ERA_COL = "era"
+TARGET_COL = "target_nomi_20"
+DATA_TYPE_COL = "data_type"
+EXAMPLE_PREDS_COL = "example_preds"
+
+MODEL_FOLDER = "models"
+MODEL_CONFIGS_FOLDER = "model_configs"
+PREDICTION_FILES_FOLDER = "prediction_files"
 
 
+def save_model(model, name):
+    try:
+        Path(MODEL_FOLDER).mkdir(exist_ok=True, parents=True)
+    except Exception as ex:
+        pass
+    pd.to_pickle(model, f"{MODEL_FOLDER}/{name}.pkl")
+
+
+def load_model(name):
+    path = Path(f"{MODEL_FOLDER}/{name}.pkl")
+    if path.is_file():
+        model = pd.read_pickle(f"{MODEL_FOLDER}/{name}.pkl")
+    else:
+        model = False
+    return model
+
+
+def save_model_config(model_config, model_name):
+    try:
+        Path(MODEL_CONFIGS_FOLDER).mkdir(exist_ok=True, parents=True)
+    except Exception as ex:
+        pass
+    with open(f"{MODEL_CONFIGS_FOLDER}/{model_name}.json", 'w') as fp:
+        json.dump(model_config, fp)
+
+
+def load_model_config(model_name):
+    path_str = f"{MODEL_CONFIGS_FOLDER}/{model_name}.json"
+    path = Path(path_str)
+    if path.is_file():
+        with open(path_str, 'r') as fp:
+            model_config = json.load(fp)
+    else:
+        model_config = False
+    return model_config
+
+
+def get_biggest_change_features(corrs, n):
+    all_eras = corrs.index.sort_values()
+    h1_eras = all_eras[:len(all_eras) // 2]
+    h2_eras = all_eras[len(all_eras) // 2:]
+
+    h1_corr_means = corrs.loc[h1_eras, :].mean()
+    h2_corr_means = corrs.loc[h2_eras, :].mean()
+
+    corr_diffs = h2_corr_means - h1_corr_means
+    worst_n = corr_diffs.abs().sort_values(ascending=False).head(n).index.tolist()
+    return worst_n
+
+
+def get_time_series_cross_val_splits(data, cv=3, embargo=12):
+    all_train_eras = data[ERA_COL].unique()
+    len_split = len(all_train_eras) // cv
+    test_splits = [all_train_eras[i * len_split:(i + 1) * len_split] for i in range(cv)]
+    # fix the last test split to have all the last eras, in case the number of eras wasn't divisible by cv
+    test_splits[-1] = np.append(test_splits[-1], all_train_eras[-1])
+
+    train_splits = []
+    for test_split in test_splits:
+        test_split_max = int(np.max(test_split))
+        test_split_min = int(np.min(test_split))
+        # get all of the eras that aren't in the test split
+        train_split_not_embargoed = [e for e in all_train_eras if not (test_split_min <= int(e) <= test_split_max)]
+        # embargo the train split so we have no leakage.
+        # one era is length 5, so we need to embargo by target_length/5 eras.
+        # To be consistent for all targets, let's embargo everything by 60/5 == 12 eras.
+        train_split = [e for e in train_split_not_embargoed if
+                       abs(int(e) - test_split_max) > embargo and abs(int(e) - test_split_min) > embargo]
+        train_splits.append(train_split)
+
+    # convenient way to iterate over train and test splits
+    train_test_zip = zip(train_splits, test_splits)
+    return train_test_zip
+
+
+def neutralize(df,
+               columns,
+               neutralizers=None,
+               proportion=1.0,
+               normalize=True,
+               era_col="era"):
+    if neutralizers is None:
+        neutralizers = []
+    unique_eras = df[era_col].unique()
+    computed = []
+    for u in unique_eras:
+        df_era = df[df[era_col] == u]
+        scores = df_era[columns].values
+        if normalize:
+            scores2 = []
+            for x in scores.T:
+                x = (scipy.stats.rankdata(x, method='ordinal') - .5) / len(x)
+                x = scipy.stats.norm.ppf(x)
+                scores2.append(x)
+            scores = np.array(scores2).T
+        exposures = df_era[neutralizers].values
+
+        scores -= proportion * exposures.dot(
+            np.linalg.pinv(exposures.astype(np.float32)).dot(scores.astype(np.float32)))
+
+        scores /= scores.std(ddof=0)
+
+        computed.append(scores)
+
+    return pd.DataFrame(np.concatenate(computed),
+                        columns=columns,
+                        index=df.index)
+
+
+def neutralize_series(series, by, proportion=1.0):
+    scores = series.values.reshape(-1, 1)
+    exposures = by.values.reshape(-1, 1)
+
+    # this line makes series neutral to a constant column so that it's centered and for sure gets corr 0 with exposures
+    exposures = np.hstack(
+        (exposures,
+         np.array([np.mean(series)] * len(exposures)).reshape(-1, 1)))
+
+    correction = proportion * (exposures.dot(
+        np.linalg.lstsq(exposures, scores, rcond=None)[0]))
+    corrected_scores = scores - correction
+    neutralized = pd.Series(corrected_scores.ravel(), index=series.index)
+    return neutralized
+
+
+def unif(df):
+    x = (df.rank(method="first") - 0.5) / len(df)
+    return pd.Series(x, index=df.index)
+
+
+def get_feature_neutral_mean(df, prediction_col):
+    feature_cols = [c for c in df.columns if c.startswith("feature")]
+    df.loc[:, "neutral_sub"] = neutralize(df, [prediction_col],
+                                          feature_cols)[prediction_col]
+    scores = df.groupby("era").apply(
+        lambda x: (unif(x["neutral_sub"]).corr(x[TARGET_COL]))).mean()
+    return np.mean(scores)
+
+
+def fast_score_by_date(df, columns, target, tb=None, era_col="era"):
+    unique_eras = df[era_col].unique()
+    computed = []
+    for u in unique_eras:
+        df_era = df[df[era_col] == u]
+        era_pred = np.float64(df_era[columns].values.T)
+        era_target = np.float64(df_era[target].values.T)
+
+        if tb is None:
+            ccs = np.corrcoef(era_target, era_pred)[0, 1:]
+        else:
+            tbidx = np.argsort(era_pred, axis=1)
+            tbidx = np.concatenate([tbidx[:, :tb], tbidx[:, -tb:]], axis=1)
+            ccs = [np.corrcoef(era_target[tmpidx], tmppred[tmpidx])[0, 1] for tmpidx, tmppred in zip(tbidx, era_pred)]
+            ccs = np.array(ccs)
+
+        computed.append(ccs)
+
+    return pd.DataFrame(np.array(computed), columns=columns, index=df[era_col].unique())
+
+
+def validation_metrics(validation_data, pred_cols, example_col, fast_mode=False):
+    validation_stats = pd.DataFrame()
+    feature_cols = [c for c in validation_data if c.startswith("feature_")]
+    for pred_col in pred_cols:
+        # Check the per-era correlations on the validation set (out of sample)
+        validation_correlations = validation_data.groupby(ERA_COL).apply(
+            lambda d: unif(d[pred_col]).corr(d[TARGET_COL]))
+
+        mean = validation_correlations.mean()
+        std = validation_correlations.std(ddof=0)
+        sharpe = mean / std
+
+        validation_stats.loc["mean", pred_col] = mean
+        validation_stats.loc["std", pred_col] = std
+        validation_stats.loc["sharpe", pred_col] = sharpe
+
+        rolling_max = (validation_correlations + 1).cumprod().rolling(window=9000,  # arbitrarily large
+                                                                      min_periods=1).max()
+        daily_value = (validation_correlations + 1).cumprod()
+        max_drawdown = -((rolling_max - daily_value) / rolling_max).max()
+        validation_stats.loc["max_drawdown", pred_col] = max_drawdown
+
+        payout_scores = validation_correlations.clip(-0.25, 0.25)
+        payout_daily_value = (payout_scores + 1).cumprod()
+
+        apy = (
+                      (
+                              (payout_daily_value.dropna().iloc[-1])
+                              ** (1 / len(payout_scores))
+                      )
+                      ** 49  # 52 weeks of compounding minus 3 for stake compounding lag
+                      - 1
+              ) * 100
+
+        validation_stats.loc["apy", pred_col] = apy
+
+        if not fast_mode:
+            # Check the feature exposure of your validation predictions
+            max_per_era = validation_data.groupby(ERA_COL).apply(
+                lambda d: d[feature_cols].corrwith(d[pred_col]).abs().max())
+            max_feature_exposure = max_per_era.mean()
+            validation_stats.loc["max_feature_exposure", pred_col] = max_feature_exposure
+
+            # Check feature neutral mean
+            feature_neutral_mean = get_feature_neutral_mean(validation_data, pred_col)
+            validation_stats.loc["feature_neutral_mean", pred_col] = feature_neutral_mean
+
+            # Check top and bottom 200 metrics (TB200)
+            tb200_validation_correlations = fast_score_by_date(
+                validation_data,
+                [pred_col],
+                TARGET_COL,
+                tb=200,
+                era_col=ERA_COL
+            )
+
+            tb200_mean = tb200_validation_correlations.mean()[pred_col]
+            tb200_std = tb200_validation_correlations.std(ddof=0)[pred_col]
+            tb200_sharpe = tb200_mean / tb200_std
+
+            validation_stats.loc["tb200_mean", pred_col] = tb200_mean
+            validation_stats.loc["tb200_std", pred_col] = tb200_std
+            validation_stats.loc["tb200_sharpe", pred_col] = tb200_sharpe
+
+        # MMC over validation
+        mmc_scores = []
+        corr_scores = []
+        for _, x in validation_data.groupby(ERA_COL):
+            series = neutralize_series(unif(x[pred_col]), (x[example_col]))
+            mmc_scores.append(np.cov(series, x[TARGET_COL])[0, 1] / (0.29 ** 2))
+            corr_scores.append(unif(x[pred_col]).corr(x[TARGET_COL]))
+
+        val_mmc_mean = np.mean(mmc_scores)
+        val_mmc_std = np.std(mmc_scores)
+        corr_plus_mmcs = [c + m for c, m in zip(corr_scores, mmc_scores)]
+        corr_plus_mmc_sharpe = np.mean(corr_plus_mmcs) / np.std(corr_plus_mmcs)
+
+        validation_stats.loc["mmc_mean", pred_col] = val_mmc_mean
+        validation_stats.loc["corr_plus_mmc_sharpe", pred_col] = corr_plus_mmc_sharpe
+
+        # Check correlation with example predictions
+        per_era_corrs = validation_data.groupby(ERA_COL).apply(lambda d: unif(d[pred_col]).corr(unif(d[example_col])))
+        corr_with_example_preds = per_era_corrs.mean()
+        validation_stats.loc["corr_with_example_preds", pred_col] = corr_with_example_preds
+
+    # .transpose so that stats are columns and the model_name is the row
+    return validation_stats.transpose()
+
+
+########################################################################################################################
+# Acknowledgement: All functions before this are provided in the official Numaria data set
 
 def download_data() -> None:
     """Downloads the latest Numerai training, validation, and live data under
     ./data
     """
-    # Create instance of NumerAPI
-    napi = numerapi.NumerAPI()
 
+    print('Downloading dataset files...')
     # Get the current round
     CURRENT_ROUND = napi.get_current_round()
 
@@ -25,55 +290,46 @@ def download_data() -> None:
         if "parquet" in file and "int8" in file:
             if "training" in file or "validation" in file:
                 napi.download_dataset(file, f"data/{file}")
-            else:
-                Path(f"data/{CURRENT_ROUND}").mkdir(exist_ok=True, parents=True)
-                napi.download_dataset(file, f"data/{CURRENT_ROUND}/{file}")
+            ## Uncomment the following when we want to actually predict on tornament data
+            # else:
+            #     Path(f"data/{CURRENT_ROUND}").mkdir(exist_ok=True, parents=True)
+            #     napi.download_dataset(file, f"data/{CURRENT_ROUND}/{file}")
+    print("Done!")
 
 
-def read_data(path: str) -> DataFrame:
-    """Read data stored in .parquet files as a dask DataFrame
+def read_learning_data(features, training_data_path='./data/numerai_training_data_int8.parquet'
+                       , validation_data_path='./data/numerai_validation_data_int8.parquet'):
+    print('Reading learning data...')
 
-    :param path: path to the parquet file
-    :return: the data in the parquet file as a DataFrame
-    """
-    df = dd.read_parquet(f'{path}')
-    return df
+    # read in just those features along with era and target columns
+    read_columns = features + [ERA_COL, DATA_TYPE_COL, TARGET_COL]
 
-def extract_minimum_learning_data(training_set, validation_set) -> \
-        (DataFrame, DataFrame, DataFrame, DataFrame):
-    """Extract minimum learning data from the provided training set and
-    validation set
+    # note: sometimes when trying to read the downloaded data you get an error about invalid magic parquet bytes...
+    # if so, delete the file and rerun the napi.download_dataset to fix the corrupted file
+    training_data = pd.read_parquet(
+        training_data_path, columns=read_columns)
 
-    :param training_set: Training dataset
-    :param validation_set: Validation dataset
-    :return: (training features, training targets,
-    validation features, validation targets)
-    """
-    # Isolate all feature and target names
-    print('Extracting minimal training data...')
-    # read the feature metadata amd get the "small" feature set
-    with open("features.json", "r") as f:
-        feature_metadata = json.load(f)
-    features = feature_metadata["feature_sets"]["small"]
-
-    # Training data
-    X_train = training_set[features]
-    y_train = training_set['target_nomi_20']
-
-    # Validation data
-    X_val = validation_set[features]
-    y_val = validation_set['target_nomi_20']
-
+    validation_data = pd.read_parquet(
+        validation_data_path,
+        columns=read_columns)
     print('Done!')
 
-    return X_train, y_train, X_val, y_val
+    return training_data, validation_data
 
 
-def make_csv(labels, predictions, model_name='model') -> None:
-    """ Export predictions as a csv file in the format wanted by numerai
-    """
-    print('Exporting Predictions to csv...')
-    data_frame = pd.DataFrame(data=predictions, index=labels, columns=["prediction"])
-    data_frame.index.name = 'id'
-    data_frame.to_csv(f'{model_name}_prediction.csv')
-    print('Done!')
+def neutralize_riskiest_features(training_data, validation_data, features, model_name, k=50):
+    # getting the per era correlation of each feature vs the target in the training set
+    all_feature_corrs = training_data.groupby(ERA_COL).apply(
+        lambda era: era[features].corrwith(era[TARGET_COL])
+    )
+    # calculate the k riskiest (highest correlated) features of the training set
+    riskiest_features = get_biggest_change_features(all_feature_corrs, k)
+
+    validation_data[f"preds_{model_name}_neutral_riskiest_{k}"] = neutralize(
+        df=validation_data,
+        columns=[f"preds_{model_name}"],
+        neutralizers=riskiest_features,
+        proportion=1.0,
+        normalize=True,
+        era_col=ERA_COL
+    )
